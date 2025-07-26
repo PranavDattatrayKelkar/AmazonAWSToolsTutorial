ce4a05052b42fd27a4f7d25e2a7237724a49d931
pranavkelkar <kelkarpranav20.pk@gmail.com>
2025-07-26 08:33:08 -0700

    Removing AWS credentials

--- src/main/java/org/example/dynamodb/DDBTableUtility.java
+++ src/main/java/org/example/dynamodb/DDBTableUtility.java
@@ -12,8 +12,8 @@ import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 public class DDBTableUtility {
     public static String tableName = "pranav-table";
     public static AwsCredentialsProvider credentialsProvider = StaticCredentialsProvider.create(
-            AwsBasicCredentials.create("AKIA4S7LOIWHQMGBHHXF",
-                    "b8mBy49AOqa6IGxPwRay2PCazezZ68T4PnSz2Sb6")
+            AwsBasicCredentials.create("Add your access key here",
+                    "Add your secret key here")
     );
     public static DynamoDbClient dbClient = DynamoDBClientFactory.createClient(credentialsProvider,
             Region.of("us-west-2"));
